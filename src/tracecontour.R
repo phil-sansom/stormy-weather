ccb5bd55df7ae4809f20ea63187eeb850ad7b8d1
ps344 <p.g.sansom@exeter.ac.uk>
2021-07-08 15:03:42 +0100

    Commented contour tracing.

--- src/tracecontour.R
+++ src/tracecontour.R
@@ -23,9 +23,16 @@ move.clockwise = function(x, x0) {
 
 trace.contour = function(x) {
   
+  ## Expand by one latitude to allow for objects on the bottom row
   x = cbind(0,x)
+  
+  ## Extract dimensions
   nx = nrow(x)
   ny = ncol(x)
+  
+  ## Find first meridian with no points in it
+  ## Need to start from leftmost point in object, this avoids finding the
+  ## right side of a wrapped object which is stored on the left.
   i0 = 0
   total = 1
   while(total > 0 & i0 <= nx) {
@@ -33,6 +40,9 @@ trace.contour = function(x) {
     total = sum(x[i0,])
   }
   i0 = (i0 - 1) %% nx + 1
+  
+  ## Find first filled point, scanning from meridians from bottom to top and
+  ## left to right
   c = c(1,1)
   skip = FALSE
   for (i in i0:nx) {
@@ -48,6 +58,8 @@ trace.contour = function(x) {
     if (skip)
       break
   } ## i
+  
+  ## Trace contour using Moore-Neighbor tracing
   p = s
   B = p
   c0 = c
@@ -58,6 +70,7 @@ trace.contour = function(x) {
   cx = (c[1] - 1) %% nx + 1
   cy = c[2]
   while(cx != s[1] | cy != s[2] | cm1x != c0[1] | cm1y != c0[2]) {
+    ## If
     if (x[cx,cy] == 1) {
       B = cbind(B,c)
       p = c
@@ -73,6 +86,7 @@ trace.contour = function(x) {
     cy = c[2]
   } ## c != s
   
+  ## Remove added latitude and return
   B[2,] = B[2,] - 1
   t(B)
   
